1ff86103205104fafac3220d9db2b8966fb2f135
hendisantika <hendisantika@yahoo.co.id>
2023-11-15 07:57:24 +0700

    #8 - Add store in User Controller 🫸🌀✏️📗🐧🐳

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,27 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        $fileName = time() . '.' . $request->image->extension();
+        $request->image->storeAs('public/images', $fileName);
+
+        $user = new User;
+        $user->name = $request->input('name');
+        $user->email = trim($request->input('email'));
+        $user->password = bcrypt($request->input('password'));
+        $user->image = $fileName;
+        $user->save();
+
+        return redirect()->route('user.index')->with([
+            'message' => 'User added successfully!',
+            'status' => 'success'
+        ]);
     }
 
     /**
